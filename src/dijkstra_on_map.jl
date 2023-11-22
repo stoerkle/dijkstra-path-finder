756c92f08f22df884b34b89f8f09c7e617a36df1
Denis Stoerkle <denis.stoerkle@cognibotics.com>
2023-11-22 11:39:04 +0100

    Added script that computes and plots the shortest path on a simple 2D map.

---
+++ src/dijkstra_on_map.jl
@@ -0,0 +1,54 @@
+using Plots
+
+include("dijkstra.jl")
+using .Dijkstra
+
+booleanmap = [
+    true true true true true
+    true true true true true
+    true false false true true
+    true true false true true
+    true true true true true
+    true true true true true
+];
+
+idx_start_node = 1
+idx_end_node = 25
+
+h, w = size(booleanmap)
+
+graph, TreeSet = convert_booleanmap2adjacencymatrix(booleanmap)
+
+path = compute_shortes_path(graph, TreeSet, idx_start_node)
+
+plot_map, plot_obs = convert_booleanmap2plotmap(booleanmap)
+
+fig = plot(
+    plot_map,
+    color=:lightgrey,
+    xlim = (0, w),
+    ylims = (-h, 0),
+    linecolor = :grey,
+    opacity=.5,
+    aspect_ratio=:equal,
+    legend = :none);
+
+plot!(fig, 
+    plot_obs,
+    color = :grey,
+    linecolor = :grey,
+    opacity = 0.5);
+
+path_points = get_coordindates_on_map.(path[idx_end_node], h, w)
+plot!(fig,
+    path_points);
+
+annotate!(fig,
+    get_coordindates_on_map(idx_start_node, h, w)[1],
+    get_coordindates_on_map(idx_start_node, h, w)[2],
+    text("S"));
+
+annotate!(fig,
+    get_coordindates_on_map(idx_end_node, h, w)[1],
+    get_coordindates_on_map(idx_end_node, h, w)[2],
+    text("E"))
